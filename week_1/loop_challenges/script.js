6d7d6b1fe6fc540a62f55178efc1d0d638d64157
Taylor Bisett <taylorbisett@gmail.com>
2021-06-29 23:19:17 -0700

    iteration 1 and 2

---
+++ week_1/loop_challenges/script.js
@@ -0,0 +1,19 @@
+// for(i=0; i<21; i++) {
+//     if(i % 2 == 1) 
+//     console.log(i);
+// }
+
+// for(i=100; i>=0; i--) {
+//     if(i % 3 == 0)
+//     console.log(i);
+// }
+
+// var arr = [4, 2.5, 1, -0.5, -2, -3.5]
+// for(var i=[0]; i < [6]; i++) {
+//     if(i = arr)
+//     console.log(i);
+// }
+
+
+
+
